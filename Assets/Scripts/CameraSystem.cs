23ba89abe17e2a0276846c148653202101992579
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-03-12 12:47:27 +0200

    Zoom

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -1,31 +1,45 @@
+using System;
 using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
 {
-    public float speed = 10;
-    
-    private void Update()
-    {
-        var input = new Vector3
-        {
-            x = Input.GetAxisRaw("Horizontal"),
-            y = 0,
-            z = Input.GetAxisRaw("Vertical")
-        };
+    private Camera cam;
+    public float scrollSpeed = 20;
 
-        transform.position += input * speed * Time.deltaTime;
+    [Range(0f,1f)]public float zoom;
+    public float minZoom = 5;
+    public float maxZoom = 20;
+    public float zoomSpeed = 5;
+    public float zoomSmoothness = 0.9f;
+
+    private float targetFOV;
+    
+    private void Start()
+    {
+        cam = Camera.main;
+        targetFOV = cam.fieldOfView;
+    }
+
+    private void LateUpdate()
+    {
+        var input = cam.ScreenToViewportPoint(Input.mousePosition);
+        var scrollDir = Vector3.zero;
         
-        //Screen.width
-        // Input.mousePosition
+        if (input.x < 0.1f) scrollDir.x = -1;
+        if (input.x > 0.9f) scrollDir.x = 1;
+        if (input.y < 0.1f) scrollDir.z = -1;
+        if (input.y > 0.9f) scrollDir.z = 1;
+        scrollDir.Normalize();
+
+        transform.position += scrollDir * scrollSpeed * Time.deltaTime;
         
-        
-        // scrollwheel ir transform forward
-        // min max apribojimai
-        // smooth
-        
-        // tower place preview 
-        // red 
-        
-        
+        // zoom
+        var wheelDir = Input.mouseScrollDelta.y;
+        zoom += wheelDir * zoomSpeed * Time.deltaTime;
+        zoom = Mathf.Clamp01(zoom);
+
+        targetFOV = Mathf.Lerp(minZoom, maxZoom, zoom);
+
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, 1 - zoomSmoothness);
     }
 }
